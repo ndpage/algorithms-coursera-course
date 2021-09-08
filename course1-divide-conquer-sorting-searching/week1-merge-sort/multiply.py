1eb0ac6bb4ccd4298001156e817f9b8b7ec0fb39
Nathan Page <33336363+ndpage@users.noreply.github.com>
2021-09-08 14:49:12 +0000

    Added code to multiply 2 numbers via additive recursion

--- course1-divide-conquer-sorting-searching/week1-merge-sort/multiply.py
+++ course1-divide-conquer-sorting-searching/week1-merge-sort/multiply.py
@@ -1,1 +1,19 @@
 
+def multiply(num1, num2):
+    if num1 == 0 or num2 == 0:
+        return 0
+    if num1 == 1:
+        return num2
+    
+    return multiply(num1-1,num2)+num2
+
+
+print("multiply.py")
+
+a = int(input("input first number: "))
+b = int(input("input second number: "))
+
+print("Product of",a,"and", b, "is", multiply(a,b))
+
+
+
