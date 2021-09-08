66778a85975f059313a78af312135c1769c9620d
Nathan Page <33336363+ndpage@users.noreply.github.com>
2021-09-08 15:48:08 +0000

    Takes two integer inputs and computes the product using Katasuba multiplication

--- course1-divide-conquer-sorting-searching/week1-merge-sort/assignment.py
+++ course1-divide-conquer-sorting-searching/week1-merge-sort/assignment.py
@@ -1,34 +1,48 @@
 # Katasuba multiplication
+# 
 
 import math
 
 def first_n_digits(num, n):
     return num // 10 ** (int(math.log(num, 10)) - n + 1)
 
-def last_n_digits(num, n):
-    return reverse_number(first_n_digits(reverse_number(num),n))
 
-def reverse_number(num):
-    # Initiate value to null  
-    revs_num = 0  
+# multiplication function using Karatsuba 
+def multiply(number1, number2):
+    if len(str(number1)) == 1 or len(str(number2)) == 1:
+        return number1*number2  
+    
+    length = max(len(str(number1)),len(str(number2))) # get the max length of number1 and number2
+    half = math.ceil(length / 2)
   
-    # reverse the integer number using the while loop  
-    while (num > 0):  
-        # Logic  
-        remainder = num % 10  
-        revs_num = (revs_num * 10) + remainder  
-        num = num // 10
-    return revs_num
+    # Determine sub numbers from each number argument  
+    a = number1 // 10**(half)
+    b = number1 % 10**(half)
+    c = number2 // 10**(half)
+    d = number2 % 10**(half)
+    
+    print(''' 
+    a {} 
+    b {} 
+    c {} 
+    d {}
+    '''.format(a,b,c,d))
 
+    # Calculate each sub product to use in final expression
+    ac = multiply(a,c)
+    bd = multiply(b,d)
+    ad_plus_bc = multiply(a+b,c+d) - ac - bd
 
-number = 1234567
-length = len(str(number))
+    return ac * 10**(2*half) + (ad_plus_bc * 10**half) + bd
 
-first = math.ceil(length/2)
+num1 = input("enter number 1: ")
+num2 = input("enter number 2: ")
 
-print(first)
-last = length - first
-print(last)
+try: 
+    num1 = int(num1)
+    num2 = int(num2)
+except:
+    print("Please enter an integer number!")
+    quit()
 
-print(first_n_digits(number, first))
-print(last_n_digits(number, last))
+print("Product is",multiply(num1,num2))
